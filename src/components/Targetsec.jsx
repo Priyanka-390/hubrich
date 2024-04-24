d31191d16154cef5d8b6c4716f86280f0d4bd5ee
Priyanka-390 <psr82220@gmail.com>
2024-04-24 11:42:23 +0530

    change in target

--- src/components/Targetsec.jsx
+++ src/components/Targetsec.jsx
@@ -1,40 +1,68 @@
 export default function Targetsec() {
-    return (
-        <>
-        <div className="bg-[#F2F8FF] overflow-x-clip~ py-14 relative">
-            <div className="container max-w-[1140px] 2xl:max-w-[1320px] px-3 mx-auto">
-                <div className="flex flex-wrap items-center flex-row -mx-3">
-                    <div className="md:w-7/12 w-full">
-                        <h2  data-aos="fade-right" className=" uppercase ff-helvetica md:text-[52px] sm:text-5xl text-3xl lg:text-end  text-center font-normal text-[#F77B0B]">TARGET <span className="text-black">group</span></h2>
-                    </div>
-                    <div  data-aos="fade-left" className="md:w-5/12 w-full px-3 flex lg:justify-end justify-center">
-                        <p className=" font-poppins md:after:left-[-14px] after:-left-2 relative max-md:pt-2 after:absolute max-md:text-center max-md:before:absolute max-md:before:w-[2px] max-md:before:bottom-0 max-md:before:bg-[#9800B0] max-md:before:top-0 max-md:before:-right-2 after:w-[2px] after:top-0 after:bottom-0 after:bg-[#9800B0] font-normal text-sm sm:text-base max-w-[350px] text-black">The coaching is aimed at married women who have already separated or are thinking of separating.</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="bg-[#F2F8FF] overflow-x-clip~ py-14 relative">
+        <div className="container max-w-[1140px] 2xl:max-w-[1320px] px-3 mx-auto">
+          <div className="flex flex-wrap items-center flex-row -mx-3">
+            <div className="md:w-7/12 w-full">
+              <h2
+                data-aos="fade-right"
+                className=" uppercase ff-helvetica md:text-[52px] sm:text-5xl text-3xl lg:text-end  text-center font-normal text-[#F77B0B]"
+              >
+                TARGET <span className="text-black">group</span>
+              </h2>
             </div>
+            <div
+              data-aos="fade-left"
+              className="md:w-5/12 w-full px-3 flex lg:justify-end justify-center"
+            >
+              <p className=" font-poppins md:after:left-[-14px] after:-left-2 relative max-md:pt-2 after:absolute max-md:text-center max-md:before:absolute max-md:before:w-[2px] max-md:before:bottom-0 max-md:before:bg-[#9800B0] max-md:before:top-0 max-md:before:-right-2 after:w-[2px] after:top-0 after:bottom-0 after:bg-[#9800B0] font-normal text-sm sm:text-base max-w-[350px] text-black">
+                The coaching is aimed at married women who have already
+                separated or are thinking of separating.
+              </p>
             </div>
-            <div className="bg-[url(/assets/images/targetbg.webp)] md:py-[108px] sm:py-20 py-14 bg-no-repeat bg-cover">
-                <div className="container relative max-w-[1140px] 2xl:max-w-[1320px] px-3 mx-auto">
-                    <div  data-aos="fade-down" className="flex flex-wrap max-md:mx-auto flex-row -mx-3">
-                        <div className="md:w-1/2 px-3 w-full">
-                            <div className="bg-[#FFFFFF1F] max-md:mx-auto hover:bg-[#003E92B2] duration-300 md:mb-8 mb-3 md:h-[187px] h-[140px] md:px-10 p-5 md:py-10 max-w-[382px] border-[#FFFFFF26] border-[1px] border-solid">
-                                <p className="text-white font-poppins font-normal sm:text-base text-sm">You have separated and have the feeling that you can never be happy again and that you will be alone forever.</p>
-                            </div>
-                            <div className="md:px-8 p-8 md:hidden block md:py-16 max-md:mx-auto md:translate-y-72 md:h-[272px] h-[200px] bg-[#003E92B2] max-w-[338px] max-md:max-w-[382px] backdrop-blur-[11px]">
-                                <p className=" font-poppins font-normal sm:text-base text-sm text-white">To understand how to let go of your partner, you need a detailed step-by-step action plan, which you will receive from me in a 1:1 coaching session.</p>
-                            </div>
-                             <div className="bg-[#FFFFFF1F] max-md:mt-3 max-md:mx-auto hover:bg-[#003E92B2] duration-300 md:h-[187px] h-[140px] md:px-10 p-5 md:py-10 max-w-[382px] border-[#FFFFFF26] border-[1px] border-solid">
-                                <p className="text-white font-poppins font-normal sm:text-base text-sm">How are you supposed to be able to let go of someone who is so entangled with your life? It's impossible, isn't it?.</p>
-                            </div>
-                        </div>
-                        <div className="md:w-1/2 px-3   hidden md:flex justify-end">
-                            <div className="px-8 py-16 mr-4 md:translate-y-72 h-[272px] bg-[#003E92B2] max-w-[338px] backdrop-blur-[11px]">
-                                <p className=" font-poppins font-normal text-base text-white">To understand how to let go of your partner, you need a detailed step-by-step action plan, which you will receive from me in a 1:1 coaching session.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[url(/assets/images/targetbg.webp)] md:py-[108px] sm:py-20 py-14 bg-no-repeat bg-cover">
+        <div className="container relative max-w-[1140px] 2xl:max-w-[1320px] px-3 mx-auto">
+          <div
+            data-aos="fade-down"
+            className="flex flex-wrap max-md:mx-auto flex-row -mx-3"
+          >
+            <div className="md:w-1/2 px-3 w-full">
+              <div className="bg-[#FFFFFF1F] max-md:mx-auto hover:bg-[#003E92B2] duration-300 md:mb-8 mb-3 md:h-[187px] h-[140px] md:px-10 p-5 md:py-10 max-w-[382px] border-[#FFFFFF26] border-[1px] border-solid">
+                <p className="text-white font-poppins font-normal sm:text-base text-sm">
+                  You have separated and have the feeling that you can never be
+                  happy again and that you will be alone forever.
+                </p>
+              </div>
+              <div className="md:px-8 p-8 md:hidden block md:py-16 max-md:mx-auto md:translate-y-72 md:h-[272px] h-[200px] bg-[#003E92B2] max-w-[338px] max-md:max-w-[382px] backdrop-blur-[11px]">
+                <p className=" font-poppins font-normal sm:text-base text-sm text-white">
+                  To understand how to let go of your partner, you need a
+                  detailed step-by-step action plan, which you will receive from
+                  me in a 1:1 coaching session.
+                </p>
+              </div>
+              <div className="bg-[#FFFFFF1F] max-md:mt-3 max-md:mx-auto hover:bg-[#003E92B2] duration-300 md:h-[187px] h-[140px] md:px-10 p-5 md:py-10 max-w-[382px] border-[#FFFFFF26] border-[1px] border-solid">
+                <p className="text-white font-poppins font-normal sm:text-base text-sm">
+                  How are you supposed to be able to let go of someone who is so
+                  entangled with your life? It's impossible, isn't it?.
+                </p>
+              </div>
             </div>
-            </>
-    )
+            <div className="md:w-1/2 px-3   hidden md:flex justify-end">
+              <div className="px-8 py-16 mr-4 md:translate-y-72 h-[272px] bg-[#003E92B2] max-w-[338px] backdrop-blur-[11px]">
+                <p className=" font-poppins font-normal text-base text-white">
+                  To understand how to let go of your partner, you need a
+                  detailed step-by-step action plan, which you will receive from
+                  me in a 1:1 coaching session.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
